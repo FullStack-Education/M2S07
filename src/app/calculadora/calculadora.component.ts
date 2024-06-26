a0d02f68f3bedaf07917553ddc3ffb27ebc50f68
Eduardo Silva <eduardo.r.da.silva@accenture.com>
2024-06-25 22:05:54 -0300

    Atividade 5

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-calculadora',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.scss'
 })
