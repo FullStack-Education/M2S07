8556c5a29b23b61a57e22d5d5f84a84c237a87e0
Eduardo Silva <eduardo.r.da.silva@accenture.com>
2024-06-25 22:00:03 -0300

    Atividade 4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'calculadora',
+    component: CalculadoraComponent
   },
   {
     path: '**',
