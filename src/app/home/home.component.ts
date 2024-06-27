b5266b3bec3d5c2cf99665cd76140bd68c3b6e3b
Eduardo Silva <eduardo.r.da.silva@accenture.com>
2024-06-26 21:32:51 -0300

    Atividade 7

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  listaAtividades = [
+    {
+      titulo: 'Calculadora',
+      imagem: 'assets/images/atividade-1.png',
+      rota: '/calculadora'
+    },
+    {
+      titulo: 'Atividade 2',
+      imagem: 'assets/images/atividade-2.png',
+      rota: ''
+    },
+    {
+      titulo: 'Atividade 3',
+      imagem: 'assets/images/atividade-3.png',
+      rota: ''
+    }
+  ];
 }
