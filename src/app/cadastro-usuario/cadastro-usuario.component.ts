373947ea980efe1c0a296798a724c4e4a1c2fb4b
Eduardo Silva <eduardo.r.da.silva@accenture.com>
2024-06-28 21:56:20 -0300

    Atividade 11

--- src/app/cadastro-usuario/cadastro-usuario.component.ts
+++ src/app/cadastro-usuario/cadastro-usuario.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-usuario',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './cadastro-usuario.component.html',
   styleUrl: './cadastro-usuario.component.scss'
 })
@@ -13,14 +14,18 @@ export class CadastroUsuarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.formCadastro = new FormGroup({
-      nome: new FormControl(''),
-      cpf: new FormControl(''),
-      email: new FormControl(''),
-      celular: new FormControl('')
+      nome: new FormControl('', Validators.required),
+      cpf: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      celular: new FormControl('', Validators.required)
     });
   }
 
   cadastrar() {
-    console.log('formCadastro: ', this.formCadastro);
+    if(this.formCadastro.valid) {
+      console.log('formCadastro: ', this.formCadastro);
+    } else {
+      this.formCadastro.markAllAsTouched();
+    }
   }
 }
