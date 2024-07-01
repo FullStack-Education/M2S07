73881218740807f2c58cc9afa5f660cb49427bdd
Eduardo Silva <eduardo.r.da.silva@accenture.com>
2024-07-01 20:52:03 -0300

    Atividade 12 - parte 2

--- src/app/usuarios/usuarios.component.ts
+++ src/app/usuarios/usuarios.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CardComponent } from '../shared/components/card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CardComponent, CommonModule],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.scss'
 })
-export class UsuariosComponent {
+export class UsuariosComponent implements OnInit {
+  textoPesquisa: string | undefined;
+  listagemUsuarios = [
+    {
+      titulo: 'Eduardo Silva',
+      imagem: 'assets/images/user.png',
+      rota: ''
+    },
+    {
+      titulo: 'José Silva',
+      imagem: 'assets/images/user.png',
+      rota: ''
+    },
+    {
+      titulo: 'José Alves',
+      imagem: 'assets/images/user.png',
+      rota: ''
+    },
+    {
+      titulo: 'Rodrigo Pereira',
+      imagem: 'assets/images/user.png',
+      rota: ''
+    }
+  ];
+  listagemUsuariosPesquisa: Array<{titulo: string, imagem: string, rota: string}> = [];
 
+  ngOnInit(): void {
+      this.listagemUsuariosPesquisa = this.listagemUsuarios;
+  }
+
+  pesquisar() {
+    if (this.textoPesquisa) {
+      this.listagemUsuariosPesquisa = this.listagemUsuarios.filter(usuario => usuario.titulo.toUpperCase().includes(this.textoPesquisa!.toUpperCase()));
+    } else {
+      this.listagemUsuariosPesquisa = this.listagemUsuarios;
+    }
+  }
 }
